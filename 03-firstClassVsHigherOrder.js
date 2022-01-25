e74c5fffb878b3177dd08cddaa379284c76eafd0
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-25 23:12:36 +0800

    Added notes about First Class Functions vs Higher Class Functions

---
+++ 03-firstClassVsHigherOrder.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// First-Class vs Higher-Order Functions
+
+/*
+First Class Functions
+- Treated as first-class citizens
+- Are simply values
+- Just another "type" of object
+
+- Store functions in variables
+- Pass functions as arguments to other functions (addEventListener)
+- Return functions from Functions 
+- Call methods on functions
+
+Higher-Order Functions
+- A function that receives another function as an argument, that returns a new function (addEventListener)
+- This is only possible because of first-class function
+
+*/
