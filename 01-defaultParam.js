2dd782d6ef83bb54fb6a671425da494020807bf6
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-25 22:25:11 +0800

    Added codes about Default Parameters

---
+++ 01-defaultParam.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const bookings = [];
+
+//Setting a default value in function
+const createBooking = function (
+  flightNumber,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //Old way of setting default value (ES5)
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNumber,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+// Skipping a default parameter using undefined
+createBooking('LH123', undefined, 1000);
