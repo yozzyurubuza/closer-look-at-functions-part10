4a56a0f0abda1d717b9cffcb5585ce95382efacd
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-26 21:23:57 +0800

    Added codes about Bind Method

---
+++ 06-07-callAndApply.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const lufthansa = {
+  airline: 'Luthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// book(23, 'Sarah Williams') //Error - this keyword is undefined
+
+//Set this keyword to eurowings
+// Call Method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply Method - needs an array of data
+// Not used anymore
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// Same as Apply Method
+book.call(swiss, ...flightData);
+
+// Bind Method
+// book.call(eurowings, 23, 'Sarah Williams');
+
+//Create a new function which sets this to eurowings always
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+// Sets the flightNum fixed to 23
+// Partial Application - Parts of the function is already applied
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+
+// Objects with Event Listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // This points to the button element, so bind is needed
+
+// Partial Application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+//   addVat = value => value + value * 23
+// Using binds creates a new function
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// Challenge
+
+const addTax2 = rate => value => console.log(`${value + value * rate}`);
+
+const addVAT2 = addTax2(0.23);
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
